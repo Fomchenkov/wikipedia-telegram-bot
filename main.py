192f7a91b8536c44326f6070e9df176d257046ce
Your Name <you@example.com>
2017-10-15 12:27:56 +0300

    Added article search

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sqlite3
+import time
 
 import wikipedia
 import telebot
@@ -10,17 +11,35 @@ from telebot import types
 import config
 
 
-def get_random_page(lang="ru"):
+class Wikipedia:
 	"""
-	Get random Wikipedia page
+	Wikipedia functions collection
 	"""
-	while True:
-		try:
-			wikipedia.set_lang(lang)
-			res = wikipedia.random(pages=1)
-			return wikipedia.page(res)
-		except wikipedia.exceptions.DisambiguationError:
-			continue
+	@staticmethod
+	def get_random_page(lang="ru"):
+		"""
+		Get random Wikipedia page
+		"""
+		while True:
+			try:
+				wikipedia.set_lang(lang)
+				res = wikipedia.random(pages=1)
+				return wikipedia.page(res)
+			except wikipedia.exceptions.DisambiguationError:
+				continue
+
+	@staticmethod
+	def find_page(title, lang="ru"):
+		"""
+		Find Wikipedia page
+		"""
+		wikipedia.set_lang(lang)
+		results = wikipedia.search(title)
+		for res in results:
+			try:
+				yield wikipedia.page(res)
+			except wikipedia.exceptions.DisambiguationError:
+				continue
 
 
 def generate_main_markup():
@@ -28,11 +47,14 @@ def generate_main_markup():
 	Generate start markup
 	"""
 	markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-	markup.add(types.KeyboardButton("Случайная статья"))
+	markup.add(types.KeyboardButton("🔎 Поиск статей"))
+	markup.add(types.KeyboardButton("👀 Случайная статья"))
 	return markup
 
 
 bot = telebot.TeleBot(config.BOT_TOKEN)
+
+ready_to_find = []
 
 
 @bot.message_handler(commands=['start'])
@@ -55,9 +77,28 @@ def text_handler(message):
 	uid = message.from_user.id
 	cid = message.chat.id
 
+	bot.send_chat_action(cid, 'typing')
+
+	# Handle find article request
+	if uid in ready_to_find:
+		sended = False
+		for page in Wikipedia.find_page(message.text):
+			text = page.title + "\n" + page.url 
+			bot.send_message(cid, text)
+			sended = True
+		if not sended:
+			text = "Нет найденых статей."
+			bot.send_message(cid, text)
+		ready_to_find.remove(uid)
+		return
+
 	# Handle keyboard buttons
-	if message.text == "Случайная статья":
-		page = get_random_page()
+	if message.text == "🔎 Поиск статей":
+		text = "Введите ключевое слово для поиска статьи"
+		ready_to_find.append(uid)
+		return bot.send_message(cid, text)
+	elif message.text == "👀 Случайная статья":
+		page = Wikipedia.get_random_page()
 		text = page.title + "\n" + page.url 
 		return bot.send_message(cid, text)
 
